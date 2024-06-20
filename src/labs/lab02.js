8faa74c4a555750c8a3c386654976c4412da09df
bryantnguyen1801 <binh.nguyen@setel.com>
2024-06-20 21:23:09 +0700

    lab02

--- src/labs/lab02.js
+++ src/labs/lab02.js
@@ -1,1 +1,56 @@
-console.log('Hello Bryant');
+const prompt = require('prompt-sync')({ sigint: true });
+
+function calculateBMI(height, weight) {
+    const bmi = weight / (height * height);
+    return bmi;
+}
+
+function returnBMIStatus(bmiValue) {
+    console.log("BMI status:");
+    if (bmiValue < 18.5) {
+        console.log("Underweight");
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+        console.log("Normal weight");
+    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+        console.log("Overweight");
+    } else {
+        console.log("Obesity");
+    }
+}
+
+function checkEvenOrOddNumber(number) {
+    if (number % 2 === 0) {
+        console.log(`${number} is an even number.`);
+    } else {
+        console.log(`${number} is an odd number.`);
+    }
+}
+
+function giveSuggestion(bmiNumber, currentWeight) {
+    console.log("Suggestion for you:");
+    if (bmiNumber < 18.5) {
+        let targetWeight = 18.5 * (currentWeight / bmiNumber);
+        console.log("Increase your weight for better health! Your target is: " + targetWeight.toFixed(2) + "kg");
+    } else if (bmiNumber > 24.9) {
+        let targetWeight = 24.9 * (currentWeight / bmiNumber);
+        console.log("Decrease your weight for better health! Your target is: " + targetWeight.toFixed(2) + "kg");
+    } else {
+        console.log("No suggestion, keep doing your work!");
+    }
+}
+
+function main() {
+    const height = parseFloat(prompt('Please input your height (m): '));
+    const weight = parseFloat(prompt('Please input your weight (kg): '));
+
+    const bmi = calculateBMI(height, weight);
+    console.log("BMI is: " + bmi.toFixed(2));
+    returnBMIStatus(bmi);
+
+    giveSuggestion(bmi, weight);
+
+    const number = parseInt(prompt("Enter a number: "));
+    checkEvenOrOddNumber(number);
+}
+
+main();
